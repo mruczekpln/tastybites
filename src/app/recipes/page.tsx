f07cd366aeaa8467c78d14d81bfa7b46d64f36b6
yummycoffee <leonwdev@gmail.com>
2023-11-11 13:56:08 +0100

    modified recipe card component a bit, pagination component

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,10 +1,18 @@
 import Input from "~/components/input";
-import { Clock, Heart, Star } from "lucide-react";
+import {
+  ChevronFirst,
+  ChevronLast,
+  ChevronLeft,
+  ChevronRight,
+  Clock,
+  Heart,
+  Star,
+} from "lucide-react";
 import Image from "next/image";
 
 function RecipeCard() {
   return (
-    <div className="flex h-48 overflow-hidden rounded-lg border-2  border-black shadow-button">
+    <div className="flex h-64 overflow-hidden rounded-lg border-2  border-black duration-300 hover:translate-x-[2px] hover:translate-y-[-2px] hover:shadow-button">
       <div className="flex h-full w-1/2 flex-col justify-between border-r-2 border-black p-4">
         <div className="flex items-start justify-between">
           <div>
@@ -13,9 +21,9 @@ function RecipeCard() {
               DINNER
             </p>
           </div>
-          <Heart></Heart>
+          <Heart absoluteStrokeWidth size={32}></Heart>
         </div>
-        <div className="flex items-end">
+        <div className="flex items-end justify-between">
           <div className="flex items-center">
             <Clock></Clock>
             <p className="ml-3 text-sm">30 min</p>
@@ -24,17 +32,58 @@ function RecipeCard() {
             <Star className="mr-1"></Star>
             <Star className="mr-1"></Star>
           </div>
-          <div className="ml-auto h-10 w-10 rounded-full border-2 border-black"></div>
+          {/* <div className="ml-auto flex h-auto w-min items-center gap-4"> */}
+          <p className="cursor-pointer whitespace-nowrap">
+            <b className="pr-2">recipe by:</b>{" "}
+            <span className="hover:underline">username 1</span>
+          </p>
+          {/* </div> */}
         </div>
       </div>
-      <div className="w-1/2 bg-yellow-100 p-4">
-        <Image
-          src="https://images.unsplash.com/photo-1571091718767-18b5b1457add?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJ1cmdlcnxlbnwwfHwwfHx8MA%3D%3D"
-          alt="recipe photo"
-          className="h-full w-full rounded-md border-2 border-black object-cover"
-          width={1000}
-          height={666}
-        ></Image>
+      {/* <div className="w-1/2 bg-yellow-100 p-4"> */}
+      <Image
+        src="https://images.unsplash.com/photo-1571091718767-18b5b1457add?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJ1cmdlcnxlbnwwfHwwfHx8MA%3D%3D"
+        alt="recipe photo"
+        className="h-full w-1/2 object-cover"
+        width={1000}
+        height={666}
+      ></Image>
+      {/* </div> */}
+    </div>
+  );
+}
+
+function Paginator() {
+  return (
+    <div className="flex w-full justify-between">
+      <div className="flex h-16 items-center">
+        <div className="flex h-full w-16 items-center justify-center hover:bg-gray-100">
+          <ChevronFirst className="rounded-lg hover:bg-gray-100"></ChevronFirst>
+        </div>
+        <div className="flex h-full w-16 items-center justify-center hover:bg-gray-100">
+          <ChevronLeft className="rounded-lg hover:bg-gray-100"></ChevronLeft>
+        </div>
+        <p className="mx-8 text-3xl font-bold">1</p>
+        <div className="flex h-full w-16 items-center justify-center hover:bg-gray-100">
+          <ChevronRight className="rounded-lg hover:bg-gray-100"></ChevronRight>
+        </div>
+        <div className="flex h-full w-16 items-center justify-center hover:bg-gray-100">
+          <ChevronLast className="rounded-lg hover:bg-gray-100"></ChevronLast>
+        </div>
+      </div>
+      <div className="flex items-center gap-4">
+        <p className="text-2xl">Showing</p>
+        <select
+          className="rounded-lg  bg-gray-100 px-2 text-xl"
+          name="per-page"
+          defaultValue={10}
+          id="per-page"
+        >
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="30">30</option>
+        </select>
+        <p className="text-2xl">per page</p>
       </div>
     </div>
   );
@@ -68,6 +117,7 @@ export default function Recipes() {
           {new Array(10).fill(null).map((_, index) => (
             <RecipeCard key={index}></RecipeCard>
           ))}
+          <Paginator></Paginator>
         </section>
       </div>
     </div>
