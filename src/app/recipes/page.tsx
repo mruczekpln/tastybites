a1c78eea5dda8f945a4e0e79039547f37c8a3706
yummycoffee <leonwdev@gmail.com>
2023-11-11 15:14:01 +0100

    sort by added, added search icon to the search bar, recipes filters

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -6,9 +6,13 @@ import {
   ChevronRight,
   Clock,
   Heart,
+  Search,
   Star,
 } from "lucide-react";
 import Image from "next/image";
+import { type ReactNode, type ComponentProps } from "react";
+import cn from "~/components/cn";
+import { Chilanka } from "next/font/google";
 
 function RecipeCard() {
   return (
@@ -89,30 +93,139 @@ function Paginator() {
   );
 }
 
+function SearchBar() {
+  return (
+    <div className="flex h-16 items-center rounded-md border-2 border-black bg-yellow-100 px-8 shadow-button">
+      <Search size={32}></Search>
+      <Input
+        // className="flex h-16 w-full items-center bg-yellow-100 pl-8 text-xl duration-300 placeholder:justify-self-center focus:bg-yellow-500 focus:placeholder:font-normal focus:placeholder:text-black"
+        border={false}
+        className="h-full w-full border-none bg-yellow-100 pl-8 outline-none placeholder:font-bold placeholder:text-black/50"
+        placeholder="what are you searching for?"
+      ></Input>
+    </div>
+  );
+}
+
+function Separator({ className }: ComponentProps<"div">) {
+  return (
+    <div className={cn(className, "h-0 border-t-[1px] border-black")}></div>
+  );
+}
+type CheckboxProps = { name: string; children: ReactNode; className?: string };
+function Checkbox({ name, children, className }: CheckboxProps) {
+  return (
+    <label htmlFor={name} className="flex w-min gap-4 text-xl">
+      <input type="checkbox" name={name} id={name} className="peer hidden" />
+      <span
+        className={cn(
+          "whitespace-nowrap peer-checked:underline peer-checked:[&>*]:fill-black",
+          className,
+        )}
+      >
+        {children}
+      </span>
+    </label>
+  );
+}
+
+function Filters() {
+  return (
+    <aside className="sticky top-24 h-min w-1/3 rounded-lg p-8">
+      <h2 className="mb-4 text-4xl font-bold">
+        <Image
+          src="/hero/arrow1.svg"
+          alt="->"
+          width={64}
+          height={64}
+          className="inline rotate-90"
+        ></Image>{" "}
+        Cooking Time
+      </h2>
+      <ul className="ml-2 flex flex-col gap-2 [&>*]:flex [&>*]:gap-4">
+        <Checkbox name="<10">less than 10 mins</Checkbox>
+        <Checkbox name="10-20">10 to 20 mins</Checkbox>
+        <Checkbox name="20-40">20 to 40 mins</Checkbox>
+        <Checkbox name="40-60">40 mins to a hour</Checkbox>
+        <Checkbox name="60-120">1 to 2 hours</Checkbox>
+        <Checkbox name=">120">more than 2 hours</Checkbox>
+      </ul>
+      <h2 className="my-4 text-4xl font-bold">
+        <Image
+          src="/hero/arrow1.svg"
+          alt="->"
+          width={64}
+          height={64}
+          className="inline rotate-90"
+        ></Image>{" "}
+        Rating
+      </h2>
+      <ul className="ml-2 flex flex-col gap-2 [&>*]:flex [&>*]:gap-4">
+        <Checkbox name="1-star" className="flex">
+          1 star
+        </Checkbox>
+        <Checkbox name="2-star" className="flex">
+          2 stars
+        </Checkbox>
+        <Checkbox name="3-star" className="flex">
+          3 stars
+        </Checkbox>
+        <Checkbox name="4-star" className="flex">
+          4 stars
+        </Checkbox>
+        <Checkbox name="5-star" className="flex">
+          5 stars
+        </Checkbox>
+      </ul>
+      <h2 className="my-4 text-4xl font-bold">
+        <Image
+          src="/hero/arrow1.svg"
+          alt="->"
+          width={64}
+          height={64}
+          className="inline rotate-90"
+        ></Image>{" "}
+        Additional
+      </h2>
+      <ul className="ml-2 flex flex-col gap-2 [&>*]:flex [&>*]:gap-4">
+        <Checkbox name="high-protein">high in protein</Checkbox>
+        <Checkbox name="low-calorie">low calorie</Checkbox>
+      </ul>
+    </aside>
+  );
+}
+
 export default function Recipes() {
   return (
     <div className="flex min-h-screen w-full flex-col items-center pt-32">
       <h1 className="font-title text-5xl font-extrabold">
-        Recipes & Cooking Ideas
+        All Recipes & Cooking Ideas
       </h1>
       <p className="py-4 text-xl">Explore various recipes and kitchen ideas.</p>
       <div className="mb-12 mt-4 flex w-full max-w-screen-2xl gap-12">
-        <aside className="w-1/3 rounded-lg bg-gray-100"></aside>
+        <Filters></Filters>
         <section className="my-4 flex w-2/3 flex-col gap-8">
-          <Input
-            type="text"
-            className="flex h-16 w-full items-center bg-yellow-100 pl-8 text-xl duration-300 placeholder:justify-self-center focus:bg-yellow-500 focus:placeholder:font-normal focus:placeholder:text-black"
-            placeholder="what are you searching for?"
-          />
-          <div className="flex w-full items-center justify-center gap-16 px-20 text-xl [&>*]:cursor-pointer">
-            {/* <div className="flex gap-8  [&>*]:underline-offset-4 [&>*]:duration-300"> */}
-            <p className="hover:underline">breakfast.</p>
-            <p className="hover:underline">lunch.</p>
-            <p className="hover:underline">dinner.</p>
-            <p className="hover:underline">apetizers.</p>
-            <p className="hover:underline">desserts.</p>
-            <p className="hover:underline">drinks.</p>
-            {/* </div> */}
+          <SearchBar></SearchBar>
+          <div className="flex w-full justify-between">
+            <div className="flex items-center justify-center gap-4 text-2xl [&>*]:cursor-pointer">
+              {/* <div className="flex gap-8  [&>*]:underline-offset-4 [&>*]:duration-300"> */}
+              <p className="hover:underline">breakfast.</p>
+              <p className="hover:underline">lunch.</p>
+              <p className="hover:underline">dinner.</p>
+              <p className="hover:underline">apetizers.</p>
+              <p className="hover:underline">desserts.</p>
+              <p className="hover:underline">drinks.</p>
+              {/* </div> */}
+            </div>
+            <div className="flex w-min items-center gap-4">
+              <b className="whitespace-nowrap">sort by</b>
+              <select name="sort-by" id="sort-by">
+                <option value="views">most views</option>
+                <option value="rating">rating</option>
+                <option value="latest">latest</option>
+                <option value="name">by name</option>
+              </select>
+            </div>
           </div>
           {new Array(10).fill(null).map((_, index) => (
             <RecipeCard key={index}></RecipeCard>
