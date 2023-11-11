069a8fa66c01114be4bc77024d2d9c3252f1029c
yummycoffee <leonwdev@gmail.com>
2023-11-11 15:14:01 +0100

    sort by added, added search icon to the search bar, recipes filters

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,15 +1,17 @@
 import { type ComponentProps } from "react";
 import cn from "./cn";
 
-type InputProps = ComponentProps<"input">;
-export default function Input(props: InputProps) {
+type InputProps = { border?: boolean } & ComponentProps<"input">;
+export default function Input({ border = true, ...props }: InputProps) {
   return (
     <input
       {...props}
       className={cn(
-        "rounded-md border-2 border-black shadow-button outline-none",
+        border && "rounded-md border-2 border-black shadow-button outline-none",
         props.className,
       )}
-    ></input>
+    >
+      {props.children}
+    </input>
   );
 }
