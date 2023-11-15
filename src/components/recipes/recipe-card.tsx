3a3cb6041354dd3043d59ea2733ee2503e704b0f
yummycoffee <leonwdev@gmail.com>
2023-11-15 19:17:26 +0100

    added /recipe/create page with form using react-hook-form & zod, todo: validation

--- src/components/recipes/recipe-card.tsx
+++ src/components/recipes/recipe-card.tsx
@@ -8,9 +8,17 @@ export default function RecipeCard() {
         <div className="flex items-start justify-between">
           <div>
             <h2 className="text-3xl leading-none">Recipe name</h2>
-            <p className="mt-3 w-min rounded-lg bg-yellow-500 p-1 px-2 text-sm font-bold text-yellow-900">
-              DINNER
-            </p>
+            <div className="mt-2 flex gap-[2px]">
+              <Star className=" fill-yellow-600 stroke-none" size={20}></Star>
+              <Star className=" fill-yellow-600 stroke-none" size={20}></Star>
+              <Star className=" fill-yellow-600 stroke-none" size={20}></Star>
+              <Star className="fill-gray-300 stroke-none" size={20}></Star>
+              <Star className="fill-gray-300 stroke-none" size={20}></Star>
+              <p className="ml-2 text-sm">3 reviews</p>
+            </div>
+            {/* <p className="mt-2 w-min rounded-lg bg-cyan-300 p-1 px-2 text-sm font-bold text-yellow-900">
+              LUNCH
+            </p> */}
           </div>
           <Heart absoluteStrokeWidth size={32}></Heart>
         </div>
@@ -19,9 +27,7 @@ export default function RecipeCard() {
             <Clock></Clock>
             <p className="ml-3 text-sm">30 min</p>
             <div className="mx-4 h-1 w-1 rounded-full bg-black"></div>
-            <Star className="mr-1"></Star>
-            <Star className="mr-1"></Star>
-            <Star className="mr-1"></Star>
+            <p className="font-medium">Easy</p>
           </div>
           <p className="cursor-pointer whitespace-nowrap">
             <b className="pr-2">recipe by:</b>{" "}
