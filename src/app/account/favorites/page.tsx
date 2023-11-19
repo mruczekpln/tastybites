aa8b8b3caa3c7d036e5982bbd3415939a34c259f
yummycoffee <leonwdev@gmail.com>
2023-11-19 21:15:12 +0100

    /account/favorites

--- src/app/account/favorites/page.tsx
+++ src/app/account/favorites/page.tsx
@@ -1,3 +1,20 @@
+import RecipeCard from "~/components/recipes/category/recipe-card";
+import Pagination from "~/components/recipes/pagination";
+
 export default function AccountFavorites() {
-  return <div>favorites</div>;
+  return (
+    <div className="w-full">
+      <h2 className="mb-2 text-4xl font-bold">Saved recipes</h2>
+      <h2 className="text-2xl">Total: 12</h2>
+      <hr className="my-4" />
+      <div className="flex w-full flex-col gap-4">
+        <RecipeCard></RecipeCard>
+        <RecipeCard></RecipeCard>
+        <RecipeCard></RecipeCard>
+        <RecipeCard></RecipeCard>
+        <RecipeCard></RecipeCard>
+        <Pagination></Pagination>
+      </div>
+    </div>
+  );
 }
