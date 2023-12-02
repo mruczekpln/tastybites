9f4f65717e47191dd939c9e6091b1b2416ad83a5
yummycoffee <leonwdev@gmail.com>
2023-12-02 15:02:25 +0100

    sync

--- src/app/(with-navbar)/recipes/category/[category]/page.tsx
+++ src/app/(with-navbar)/recipes/category/[category]/page.tsx
@@ -1,34 +1,88 @@
+import { eq, sql, type SQL } from "drizzle-orm";
 import CategoryList from "~/components/recipes/category/category-list";
 import Filters from "~/components/recipes/category/filters";
-import Pagination from "~/components/recipes/pagination";
-import RecipeCard from "~/components/recipes/recipe-card";
 import SearchBar from "~/components/recipes/category/search-bar";
 import SortBy from "~/components/recipes/category/sort-by";
+import Pagination from "~/components/recipes/pagination";
 import RouteDisplay from "~/components/recipes/path-display";
+import RecipeCard from "~/components/recipes/recipe-card";
 import { CATEGORIES } from "~/lib/constants";
+import { db } from "~/server/db";
+import { recipeLikes, recipeReviews, recipes, users } from "~/server/db/schema";
 import { type RecipeCategory } from "~/types";
 
-export default function Category({
+const getRecipes =
+  // cache(
+  async (category: RecipeCategory) => {
+    const where: SQL[] = [];
+
+    if (category !== "all") where.push(eq(recipes.category, category));
+
+    const recipeList = await db
+      .select({
+        id: recipes.id,
+        name: recipes.name,
+        difficultyLevel: recipes.difficultyLevel,
+        cookingTime: recipes.cookingTime,
+        username: users.name,
+        like_count: sql`COALESCE(COUNT(${recipeLikes.id}), 0)`,
+        review_count: sql`COALESCE(COUNT(${recipeReviews.id}), 0)`,
+      })
+      .from(recipes)
+      .leftJoin(users, eq(recipes.userId, users.id))
+      .leftJoin(recipeReviews, eq(recipes.id, recipeReviews.recipeId))
+      .leftJoin(recipeLikes, eq(recipes.id, recipeLikes.recipeId))
+      .groupBy(recipes.id, users.name);
+
+    // .from(recipes)
+    // .leftJoin(recipeLikes, eq(recipes.id, recipeLikes.recipeId))
+    // .leftJoin(recipeReviews, eq(recipes.id, recipeReviews.recipeId))
+    // .leftJoin(users, eq(recipes.userId, users.id))
+    // .where(and(...where))
+    // .groupBy(
+    //   recipes.id,
+    //   // recipeLikes.id,
+    //   // recipeReviews.id,
+    //   // recipes.instructions,
+    //   // recipes.userId,
+    //   // recipes.name,
+    //   // recipes.description,
+    //   // recipes.category,
+    //   // recipes.cookingTime,
+    //   // recipes.difficultyLevel,
+    //   // recipes.createdAt,
+    // );
+
+    console.log(recipeList);
+
+    return recipeList;
+  };
+//   undefined,
+//   { revalidate: 5 },
+// );
+
+export default async function Category({
   params,
 }: {
   params: { category: RecipeCategory };
 }) {
-  const currentCategory = CATEGORIES[params.category];
+  const recipeList = await getRecipes(params.category);
+  const currentCategoryDetails = CATEGORIES[params.category];
 
   return (
     <div className="mx-auto flex min-h-screen w-full max-w-screen-2xl flex-col items-center pt-32">
       <RouteDisplay
         arr={[
           { displayedName: "recipes", href: "/recipes" },
-          { displayedName: currentCategory.name },
+          { displayedName: currentCategoryDetails.name },
         ]}
       ></RouteDisplay>
       <div className="my-4">
         <h1 className="text-center font-title text-5xl font-extrabold">
-          {currentCategory.titleText}
+          {currentCategoryDetails.titleText}
         </h1>
         <p className="py-4 text-center text-xl">
-          {currentCategory.subtitleText}
+          {currentCategoryDetails.subtitleText}
         </p>
       </div>
       <SearchBar></SearchBar>
@@ -39,8 +93,8 @@ export default function Category({
       <div className="mb-12 mt-8 flex w-full max-w-screen-2xl gap-12">
         <Filters></Filters>
         <section className="flex w-2/3 flex-col gap-8">
-          {new Array(10).fill(null).map((_, index) => (
-            <RecipeCard key={index}></RecipeCard>
+          {recipeList.map((recipe, index) => (
+            <RecipeCard data={recipe} key={index}></RecipeCard>
           ))}
           <Pagination></Pagination>
         </section>
--- src/components/recipes/recipe-card.tsx
+++ src/components/recipes/recipe-card.tsx
@@ -1,36 +1,66 @@
-import { Heart, Clock, Star } from "lucide-react";
+import { Clock, Heart, Star } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function RecipeCard() {
+type RecipeCardProps = {
+  data: {
+    id: string;
+    name: string;
+    // createdAt: Date;
+    // description: string;
+    // userId: string;
+    // instructions: string;
+    // category: string;
+    cookingTime: number;
+    difficultyLevel: string;
+    likeCount: number;
+  };
+};
+export default function RecipeCard({
+  data: {
+    id,
+    name,
+    difficultyLevel,
+    cookingTime,
+    // createdAt,
+  },
+}: RecipeCardProps) {
   return (
-    <div className="flex h-48 cursor-pointer overflow-hidden rounded-lg border-2 border-black bg-white duration-300 hover:translate-x-[2px] hover:translate-y-[-2px] hover:shadow-button">
+    <Link
+      href={`/recipes/${id}`}
+      prefetch={false}
+      className="flex h-48 cursor-pointer overflow-hidden rounded-lg border-2 border-black bg-white duration-300 hover:translate-x-[2px] hover:translate-y-[-2px] hover:shadow-button"
+    >
       <div className="flex h-full w-1/2 flex-col justify-between border-r-2 border-black p-4">
         <div className="flex items-start justify-between">
           <div>
-            <h2 className="text-3xl leading-none">Recipe name</h2>
+            <h2 className="text-3xl leading-none">{name}</h2>
             <div className="mt-2 flex gap-[2px]">
               <Star className=" fill-yellow-600 stroke-none" size={20}></Star>
               <Star className=" fill-yellow-600 stroke-none" size={20}></Star>
               <Star className=" fill-yellow-600 stroke-none" size={20}></Star>
               <Star className="fill-gray-300 stroke-none" size={20}></Star>
               <Star className="fill-gray-300 stroke-none" size={20}></Star>
-              <p className="ml-2 text-sm">3 reviews</p>
+              <p className="ml-2 text-sm">reviewCount</p>
             </div>
             {/* <p className="mt-2 w-min rounded-lg bg-cyan-300 p-1 px-2 text-sm font-bold text-yellow-900">
               LUNCH
             </p> */}
           </div>
           <div className="flex items-center gap-2">
-            <p>32</p>
+            <p>likesAmount</p>
             <Heart absoluteStrokeWidth size={32}></Heart>
           </div>
         </div>
         <div className="flex items-end justify-between">
           <div className="flex items-center">
             <Clock></Clock>
-            <p className="ml-3 text-sm">30 min</p>
+            <p className="ml-3 text-sm">{cookingTime} min</p>
             <div className="mx-4 h-1 w-1 rounded-full bg-black"></div>
-            <p className="font-medium">Easy</p>
+            <p className="font-medium">
+              {difficultyLevel.charAt(0).toUpperCase() +
+                difficultyLevel.slice(1)}
+            </p>
           </div>
           <p className="cursor-pointer whitespace-nowrap">
             <b className="pr-2">recipe by:</b>{" "}
@@ -45,6 +75,6 @@ export default function RecipeCard() {
         width={1000}
         height={666}
       ></Image>
-    </div>
+    </Link>
   );
 }
